328af088624e986b387966aea8d3f50ace1b3e14
Luis Diego Ruiz Bautista <51675462+luis290613@users.noreply.github.com>
2022-07-28 15:23:21 -0500

    Versión 1.0 tabla que limita selecciones por secciones
    UITableView que limita la selecciones de celdas pero por secciones en su primera versión

--- tableSelectMaxNumber/ViewController.swift
+++ tableSelectMaxNumber/ViewController.swift
@@ -18,8 +18,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var lblNumberMaxSelec: UILabel!
     
-    var listsAddonsSelec : [Int] = []
-    var addOns : [AddOns] = []
+    var listsAddonsOne : [Int] = []
+    var listsAddonsTwo : [Int] = []
+    var titleSecction : [String] = []
+    var addOnsOne : [AddOns] = []
+    var addOnsTwo : [AddOns] = []
     let maxAddonsSelec : Int = 2
     
     override func viewDidLoad() {
@@ -30,26 +33,36 @@ class ViewController: UIViewController {
     
     func setupView() {
         /// cargando lista
-        addOns = [
-            AddOns(id: 1, name: "1 inka kola", check: false),
+        addOnsOne = [
+            AddOns(id: 1, name: "1 inka kola", check: true),
             AddOns(id: 2, name: "2 coca kola", check: false),
-            AddOns(id: 3, name: "3 pepsi kola", check: false),
-            AddOns(id: 4, name: "4 chuchi kola", check: true),
-            AddOns(id: 5, name: "5 chichu kola", check: false),
-            AddOns(id: 6, name: "6 pepsi kola", check: false),
-            AddOns(id: 7, name: "7 chuchi kola", check: false),
-            AddOns(id: 8, name: "8 chichu kola", check: false),
-            AddOns(id: 9, name: "9 pepsi kola", check: false)
+            AddOns(id: 3, name: "3 pepsi kola", check: true),
+            AddOns(id: 4, name: "4 chuchi kola", check: false),
+            AddOns(id: 5, name: "5 chichu kola", check: false)
         ]
+        
+        addOnsTwo = [
+            AddOns(id: 11, name: "11 inka kola", check: false),
+            AddOns(id: 22, name: "22 coca kola", check: true),
+            AddOns(id: 33, name: "33 pepsi kola", check: false)
+        ]
+        
+        titleSecction = ["Seccion uno", "Seccion dos"]
         
         /// registrando celda
         let nib = UINib(nibName: "addonsTableViewCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "addonsTableViewCell")
         
         /// cargando data seleccionada
-        addOns.forEach { item in
+        addOnsOne.forEach { item in
             if item.check {
-                listsAddonsSelec.append(item.id)
+                listsAddonsOne.append(item.id)
+            }
+        }
+        
+        addOnsTwo.forEach { item in
+            if item.check {
+                listsAddonsTwo.append(item.id)
             }
         }
         
@@ -60,8 +73,25 @@ class ViewController: UIViewController {
     
 }
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return titleSecction.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return titleSecction[section]
+        } else {
+            return titleSecction[section]
+        }
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return addOns.count
+        if section == 0 {
+            return addOnsOne.count
+        } else {
+            return addOnsTwo.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -70,41 +100,79 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
             return UITableViewCell()
         }
         
-        let item = addOns[indexPath.row]
-        
-        item.check ? cell.config(name: item.name, state: .check) : cell.config(name: item.name, state: .uncheck)
+        if indexPath.section == 0 {
+            let item = addOnsOne[indexPath.row]
+            item.check ? cell.config(name: item.name, state: .check) : cell.config(name: item.name, state: .uncheck)
 
-        /// se aplica un estilo difetente para las celdas no seleccionadas si se supera el número máximo de productos seleccionados
-        if listsAddonsSelec.count >= maxAddonsSelec {
-            if !item.check {
-                cell.configState(state: .bloqueado)
+            /// se aplica un estilo difetente para las celdas no seleccionadas si se supera el número máximo de productos seleccionados
+            if listsAddonsOne.count >= maxAddonsSelec {
+                if !item.check {
+                    cell.configState(state: .bloqueado)
+                }
+            }
+        } else {
+            let item = addOnsTwo[indexPath.row]
+            item.check ? cell.config(name: item.name, state: .check) : cell.config(name: item.name, state: .uncheck)
+
+            /// se aplica un estilo difetente para las celdas no seleccionadas si se supera el número máximo de productos seleccionados
+            if listsAddonsTwo.count >= maxAddonsSelec {
+                if !item.check {
+                    cell.configState(state: .bloqueado)
+                }
             }
         }
         
         return cell
+        
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let item = self.addOns[indexPath.row]
-        if item.check {
-            /// removemos del objeto
-            if let index = listsAddonsSelec.firstIndex(of: item.id) {
-                listsAddonsSelec.remove(at: index)
-                self.addOns[indexPath.row].check = false
-                tableView.reloadData()
-            }
-        } else {
-            if listsAddonsSelec.count >= maxAddonsSelec {
-                print("ya tienes \(maxAddonsSelec) pendej#!!!")
+        
+        if indexPath.section == 0 {
+            let item = self.addOnsOne[indexPath.row]
+            
+            if item.check {
+                /// removemos del objeto
+                if let index = listsAddonsOne.firstIndex(of: item.id) {
+                    listsAddonsOne.remove(at: index)
+                    self.addOnsOne[indexPath.row].check = false
+                    tableView.reloadData()
+                }
             } else {
-                /// agregamos al nuevo array
-                listsAddonsSelec.append(item.id)
-                self.addOns[indexPath.row].check = true
-                tableView.reloadData()
+                if listsAddonsOne.count >= maxAddonsSelec {
+                    print("ya tienes \(maxAddonsSelec) pendej#!!!")
+                } else {
+                    /// agregamos al nuevo array
+                    listsAddonsOne.append(item.id)
+                    self.addOnsOne[indexPath.row].check = true
+                    tableView.reloadData()
+                }
+            }
+            
+        } else {
+            let item = self.addOnsTwo[indexPath.row]
+            
+            if item.check {
+                /// removemos del objeto
+                if let index = listsAddonsTwo.firstIndex(of: item.id) {
+                    listsAddonsTwo.remove(at: index)
+                    self.addOnsTwo[indexPath.row].check = false
+                    tableView.reloadData()
+                }
+            } else {
+                if listsAddonsTwo.count >= maxAddonsSelec {
+                    print("ya tienes \(maxAddonsSelec) pendej#!!!")
+                } else {
+                    /// agregamos al nuevo array
+                    listsAddonsTwo.append(item.id)
+                    self.addOnsTwo[indexPath.row].check = true
+                    tableView.reloadData()
+                }
             }
         }
-        
-        print("check: \(listsAddonsSelec)")
+        print("-------------------------------------")
+        print("check ONE: \(listsAddonsOne)")
+        print("check TWO: \(listsAddonsTwo)")
     }
     
 }
